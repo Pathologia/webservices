8ebdc54ea144b3e7ad120b253b9361d0c618f80b
Lucas GURY <contact.gury@gmail.com>
2022-12-14 09:34:42 +0100

    ajout des modifs DP

--- webapp/models.py
+++ webapp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class result(models.Model):
     Id_Patient=models.CharField(max_length=10)
     Prediction=models.IntegerField()
+    Type=models.TextField()
     
 def _str_(self):
     return self.Id_Patient
